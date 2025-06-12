9f216ab353afb3562b44738f5582205bde1da545
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2025-06-12 13:33:57 +0500

    Added Blog List page, Comments page & Add Blog page.

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,1 +1,109 @@
-export default function AddBlog() {}
+import { useEffect, useRef, useState } from "react";
+import { assets, blogCategories } from "../../assets/assets";
+import Quill from "quill";
+
+export default function AddBlog() {
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState("");
+  const [subTitle, setSubTitle] = useState("");
+  const [category, setCategory] = useState("Startup");
+  const [isPublished, setIsPublished] = useState(false);
+
+  const generateAiDescription = () => {};
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, { theme: "snow" });
+    }
+  }, []);
+  return (
+    <section className="flex-1 bg-blue-50/50 text-gray-600 ">
+      <form className="h-full overflow-scroll" onSubmit={onSubmitHandler}>
+        <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
+          <p>Upload thumbnail</p>
+          <label htmlFor="image">
+            <img
+              src={!image ? assets.upload_area : URL.createObjectURL(image)}
+              alt=""
+              className="mt-2 h-16 rounded cursor-pointer"
+            />
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              name="image"
+              id="image"
+              hidden
+              required
+            />
+          </label>
+
+          <p className="mt-4">Blog Title</p>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
+          />
+
+          <p className="mt-4">Sub Title</p>
+          <input
+            type="text"
+            onChange={(e) => setSubTitle(e.target.value)}
+            value={subTitle}
+            className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
+          />
+
+          <p className="mt-4">Blog Description</p>
+          <div className="relative max-w-lg h-74 pb-16 sm:pb-10 pt-2">
+            <div ref={editorRef}></div>
+            <button
+              className="absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer"
+              type="button"
+              onClick={() => generateAiDescription}
+            >
+              Generate AI Description
+            </button>
+          </div>
+          <p className="mt-4">Choose Blog Category</p>
+          <select
+            onChange={(e) => setCategory(e.target.value)}
+            name="category"
+            className="mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded"
+          >
+            <option value="">Select category</option>
+            {blogCategories.map((item, index) => {
+              return (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              );
+            })}
+          </select>
+
+          <div className="flex gap-2 mt-4">
+            <p>Publish Now</p>
+            <input
+              type="checkbox"
+              checked={isPublished}
+              className="scale-125 cursor-pointer"
+              onChange={(e) => setIsPublished(e.target.checked)}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm"
+          >
+            Add Blog
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+}
--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -1,1 +1,57 @@
-export default function ListBlog() {}
+import { useEffect, useState } from "react";
+import { assets, blog_data } from "../../assets/assets";
+import TableItem from "../../components/admin/TableItem";
+
+export default function ListBlog() {
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    setBlogs(blog_data);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <section className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+      <h1>All Blogs</h1>
+
+      <div className="relative max-w-4xl h-4/5 mt-4 overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-600 text-left uppercase">
+            <tr>
+              <th scope="col" className="px-2 py-4 xl:px-6">
+                #
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Blog Title
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                Date
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                Status
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogs.map((blog, index) => {
+              return (
+                <TableItem
+                  key={blog._id}
+                  blog={blog}
+                  fetchBlogs={fetchBlogs}
+                  index={index + 1}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+}
